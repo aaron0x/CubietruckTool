9daf70920a7e44d3405c323695b4bf9a22f6f7d0
aaron <aaron@aaron-office.(none)>
2014-12-30 13:46:16 +0800

    extract command as parameter.

--- dirWatch/dir_watch.cpp
+++ dirWatch/dir_watch.cpp
@@ -7,36 +7,76 @@
 #include <stdexcept>
 #include <sstream>
 #include <errno.h>
+#include <unistd.h>
 
 using namespace std;
 
 bool run = true;
 
-void   subscribeSignals();
-void   quitMonitor(int sigNum);
-int    initializeInotify(const char* path);
-void   keepMonitorDir(int fd);
-string toStr(int number);
+// set values of -d/-f to dir/command.
+bool   parseParameters(int argc, char** const argv, char** dir, char** command);
 
-int main(int argc, char** arvg) {
-   if (argc != 2) {
-      cerr << "Usage: program dir_path\n";
+void   subscribeSignals();
+
+// exit program graceful when catching a signal.
+void   quitMonitor(int sigNum);
+
+int    initializeInotify(const char* path);
+
+// start to watch dir infinitely. excute command when dir changing.
+void   keepMonitorDir(int fd, const char* command);
+
+string&& toStr(int number);
+
+int main(int argc, char** argv) {
+   char* dir     = NULL;
+   char* command = NULL;
+
+   if (!parseParameters(argc, argv, &dir, &command)) {
+      cerr << "Usage: program -d dir_path -c command_for_dir_change\n";
       return EXIT_FAILURE;
    }
 
    try {
-      int fd = initializeInotify(arvg[1]);
+      int fd = initializeInotify(dir);
       subscribeSignals();
-      keepMonitorDir(fd);
+      keepMonitorDir(fd, command);
       close(fd);
    } catch (const exception& e) {
-      cerr << e.what();
+      cerr << e.what() << endl;
       return EXIT_FAILURE;  
    }
 
-   cout << "quit gracefully\n";
-
    return EXIT_SUCCESS;
+}
+
+bool parseParameters(int argc, char** const argv, char** dir, char** command) {
+   int   o = -1;
+   char* d = NULL;
+   char* c = NULL;
+
+   while((o = getopt(argc, argv, "d:c:")) != -1) {
+      switch(o) {
+         case 'd':
+            d = optarg;
+            break;
+         case 'c':
+            c = optarg;
+            break;
+         default:
+            return false;
+            break;
+      }
+   }
+
+   if (!d || !c) {
+      return false;
+   }
+
+   *dir     = d;
+   *command = c;
+
+   return true;
 }
 
 void subscribeSignals() {
@@ -47,7 +87,6 @@ void subscribeSignals() {
 }
 
 void quitMonitor(int sigNum) {
-   cout << "sig receiver " << sigNum << endl;
    run = false;
 }
 
@@ -65,23 +104,26 @@ int initializeInotify(const char* path) {
    return fd;
 }
 
-void keepMonitorDir(int fd) {
+void keepMonitorDir(int fd, const char* command) {
    while (run) {
       char buf[sizeof(struct inotify_event) + NAME_MAX + 1] = "";
-      int rc = read(fd, buf, sizeof(buf));
-      if (rc == EIO) {
+      int readRc = read(fd, buf, sizeof(buf));
+      if (readRc == EIO) {
          throw runtime_error("read inotify fd error");
       }
 
-      // turn on led.
-      cout << "qqq\n";
+      // only excute command when real event happened.
+      if (readRc > 0) {
+         int commandRc = system(command);
+         if (commandRc == -1 || WEXITSTATUS(commandRc) != 0) {
+            throw runtime_error("command failed, return " + toStr(WEXITSTATUS(commandRc)));
+         }   
+      }
    }
-
-   cout << "quit monitor\n";
 }
 
-string toStr(int number) {
+string&& toStr(int number) {
    stringstream ss;
    ss << number;
-   return ss.str();
+   return move(ss.str());
 }
